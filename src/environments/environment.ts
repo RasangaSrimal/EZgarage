f3969e3aac7394b738e88d27c1f9242b3f8c00e5
Tharindu Rasanga <trasanga95@gmail.com>
2022-05-07 17:14:15 +0530

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCx6Ftc5BrIH7D10U6uYz3vGpN7FIsVKc8",
+    authDomain: "ezgarage-349506.firebaseapp.com",
+    projectId: "ezgarage-349506",
+    storageBucket: "ezgarage-349506.appspot.com",
+    messagingSenderId: "1035636604668",
+    appId: "1:1035636604668:web:bc4188c658dc2200d72179",
+    measurementId: "G-P05B84KKD6"
+  },
 };
 
 /*
